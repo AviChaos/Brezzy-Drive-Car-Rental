37e0eec22253effa5018c6367a42af43f725ee95
AviChaos <avishd754@gmail.com>
2026-06-20 14:22:57 +0530

    initial commit

--- server/server.js
+++ server/server.js
@@ -6,10 +6,13 @@ import connectDB from "./configs/db.js";
 import userRouter from "./routes/userRoutes.js";
 import ownerRouter from "./routes/ownerRoutes.js";
 import bookingRouter from "./routes/bookingRoutes.js";
+import path from 'path'
+import { fileURLToPath } from 'url'
 
 // Initialize Express App
 const app = express()
-
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 // Connect Database
 await connectDB()
 
@@ -30,4 +33,19 @@ app.use('/api/owner', ownerRouter)
 app.use('/api/bookings', bookingRouter)
 
 const PORT = process.env.PORT || 3000;
+const clientPath = path.join(
+    __dirname,
+    '../client/dist'
+)
+app.use(
+    express.static(clientPath)
+)
+app.get('*',(req,res)=>{
+    res.sendFile(
+        path.join(
+            clientPath,
+            'index.html'
+        )
+    )
+})
 app.listen(PORT, ()=> {console.log(`Server running on port ${PORT}`)})
