621a55f242d673059250923e6ea64d0729d37845
AviChaos <avishd754@gmail.com>
2026-06-20 15:17:03 +0530

    initial commit

--- server/server.js
+++ server/server.js
@@ -6,44 +6,33 @@ import connectDB from "./configs/db.js";
 import userRouter from "./routes/userRoutes.js";
 import ownerRouter from "./routes/ownerRoutes.js";
 import bookingRouter from "./routes/bookingRoutes.js";
-import path from 'path'
-import { fileURLToPath } from 'url'
 
-// Initialize Express App
-const app = express()
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
-// Connect Database
-await connectDB()
+const app = express();
 
-// Middleware
-const allowedOrigins=[ 'https://brezzy-drive-car-rental-jftz.vercel.app', 'http://localhost:5173' ]
-app.use(cors({
-origin:true,
-credentials:true
-}));
+await connectDB();
+
+const allowedOrigins = [
+  "https://brezzy-drive-car-rental-jftz.vercel.app",
+  "http://localhost:5173",
+];
+
+app.use(
+  cors({
+    origin: allowedOrigins,
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 
-app.get('/', (req, res)=> res.send("Server is running"))
-app.use('/api/user', userRouter)
-app.use('/api/owner', ownerRouter)
-app.use('/api/bookings', bookingRouter)
+app.get("/", (req, res) => res.send("Server is running"));
+
+app.use("/api/user", userRouter);
+app.use("/api/owner", ownerRouter);
+app.use("/api/bookings", bookingRouter);
 
 const PORT = process.env.PORT || 3000;
-const clientPath = path.join(
-    __dirname,
-    '../client/dist'
-)
-app.use(
-    express.static(clientPath)
-)
-app.get('*',(req,res)=>{
-    res.sendFile(
-        path.join(
-            clientPath,
-            'index.html'
-        )
-    )
-})
-app.listen(PORT, ()=> {console.log(`Server running on port ${PORT}`)})
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
